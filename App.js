b4e72d16e994f496af0f2f37c11378d6823e6e76
intangible-media <steven@intangiblemedia.com>
2022-08-15 09:44:25 -0700

    minor change

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! something else</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <Text>HEYYYYYY</Text>
+      </View>
+      <Text style={styles.bodyText}>
+        Open up App.js to start working on your app! something else new here
+      </Text>
+      <TouchableHighlight
+        onPress={() => {
+          console.log("Hello wrold");
+        }}
+      >
+        <Text>Hello world there</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -13,8 +25,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: 200,
+  },
+  bodyText: {
+    fontSize: 40,
+    color: "red",
   },
 });
